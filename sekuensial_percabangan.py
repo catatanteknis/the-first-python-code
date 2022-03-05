027b4e690f70736aa3c51954a58b9d60bc7fcdec
nthool <roni.wijaya.2728@gmail.com>
2022-03-05 19:35:19 +0700

    sekuensial & percabangan

--- sekuensial_percabangan.py
+++ sekuensial_percabangan.py
@@ -4,15 +4,15 @@ Ini adalah project pertama menggunakan phyton
 print("Hello World!")
 print("My name is Roni Wijaya")
 
-total_susu = 10
-total_telur = 50
+total_susu = 2
+total_telur = 10
 
 print(f"Total susu = {total_susu} botol")
 print(f"Total Telur = {total_telur} butir")
-if total_susu > 3:
-    if total_telur > 0:
-        print("Beli 3 botol susu dan 5 telur")
-    else:
-        print("Cukup beli 3 botol susu saja")
+if total_susu > 3 and total_telur != 0:
+    #if total_telur > 0:
+    print("Beli 3 botol susu dan 5 telur")
+    #else:
+    #    print("Cukup beli 3 botol susu saja")
 else:
     print("Tidak usah membeli susu")
