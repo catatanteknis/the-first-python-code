a18c48089e226952b2a97b225eba9d4515dc8075
nthool <roni.wijaya.2728@gmail.com>
2022-03-05 19:33:54 +0700

    perulangan for & while

---
+++ Perulangan.py
@@ -0,0 +1,15 @@
+# perulangan FOR
+j_buku = 10
+j_baca = 0
+
+for j_baca in range(1, j_buku+1):
+    print(f"Ibu saya sudah selesai membaca {j_baca} buku")
+
+# perulangan while
+s_baca = 0
+
+while s_baca < j_buku:
+    s_baca += 1
+    print(f"Sekarang sudah membaca {s_baca} buku")
+else:
+    print(f"Sudah membaca {s_baca} buku")
