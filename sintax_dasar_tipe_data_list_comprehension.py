1b4149e65787fa1d9783ffa6e4da9ea7b7074014
nthool <roni.wijaya.2728@gmail.com>
2022-03-07 14:54:39 +0700

    list comprehension

--- sintax_dasar_tipe_data_list_comprehension.py
+++ sintax_dasar_tipe_data_list_comprehension.py
@@ -7,6 +7,9 @@ daftar_buku = [1001, 'single', 'the lord of the ring', -3.14, '4DX', 'second cha
 print('Tampilkan data variable daftar_buku:')
 print(daftar_buku)
 
+# Mengcopy seluruh element list pada variable daftar_buku ke daftar_buku_baru
+daftar_buku_baru = daftar_buku[:] #START:END atau bisa juga menggunakan step [0::2]
+
 # Menghapus element menggunakan del
 del daftar_buku[2]
 print('\nIsi data dari variable daftar_buku:')
@@ -28,3 +31,6 @@ for i in range(0, len(daftar_buku)):
 del daftar_buku[:] #START:END
 print('\nIsi data dari variable daftar_buku:')
 print(daftar_buku)
+
+print('\nIsi data dari variable daftar_buku_baru')
+print(daftar_buku_baru)
