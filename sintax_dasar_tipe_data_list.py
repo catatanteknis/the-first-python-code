96aa0fd5c51916256f67a9ef35c544a2bf5662f0
nthool <roni.wijaya.2728@gmail.com>
2022-03-07 11:25:31 +0700

    type data list

--- sintax_dasar_tipe_data_list.py
+++ sintax_dasar_tipe_data_list.py
@@ -14,14 +14,23 @@ print('\nTampilkan isi daftar_buku berdasarkan index tertentu:')
 print(daftar_buku[0])
 print(daftar_buku[2])
 
-# Menambahkan data pada list menggunakan append dan berbeda type data
+# Menambahkan data pada list menggunakan .append(), berbeda type data di ujung index list
 daftar_buku.append(-3.14)
+
+# Mengganti data pada list di index tertentu
+daftar_buku[0] = 1001
+
+# Mengambil data dari list dan dipindahkan ke variable lain menggunakan .pop()
+# apabila .pop() tidak menggunakan parameter maka data yang diambil, yang paling ujung
+# parameter .pop() bisa menggunakan minus (-)
+ambil_buku = daftar_buku.pop(2)
+print(f'\nBuku yang di ambil dari daftar list = {ambil_buku}')
 
 print('\nProses semua dengan for in range dengan bantuan len():')
 for i in range(0, len(daftar_buku)):
     print(f'{i+1}. {daftar_buku[i]}')
 
-# menghapus semua data pada list
+# menghapus semua data pada list menggunakan .clear()
 daftar_buku.clear()
 print('\nIsi data dari variable daftar_buku:')
 print(daftar_buku)
